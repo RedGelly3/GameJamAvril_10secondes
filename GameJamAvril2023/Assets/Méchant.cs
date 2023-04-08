a942c4f881e0a8386845b66002559d3887fe20e9
Théotim <theotim.mayeur@univ-nantes.fr>
2023-04-08 12:17:32 +0200

    ajout fichier méchant

---
+++ GameJamAvril2023/Assets/Méchant.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Méchant : MonoBehaviour
+{
+    public float vitesse;
+    public Transform[] balises;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
